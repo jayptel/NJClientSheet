bd1253246d444e4475940ca8c5a7f6cd865ede71
Acer <Acer@DESKTOP-02PQJEJ>
2023-09-25 17:01:41 +0530

    CompleteViewItem

--- app/src/main/java/com/rhythm/njclienttsheet/MainActivity.java
+++ app/src/main/java/com/rhythm/njclienttsheet/MainActivity.java
@@ -19,9 +19,14 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.rhythm.njclienttsheet.models.Item;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,8 +34,11 @@ public class MainActivity extends AppCompatActivity {
    // private Button buttonSend;
     //private EditText editTextName, editTextDescription, editTextAge;
    private AddItemFragment addItemFragment;
+   private ItemListFragment itemListFragment;
+    private RequestQueue requestQueue;
     private boolean isAddItemVisible = false; // To track the visibility of the Add Item fragment
-
+    private boolean isViewItemVisible = false;
+    private List<Item> itemList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,10 +67,30 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
+        requestQueue = Volley.newRequestQueue(this);
         // TODO: Implement OnClickListener for other buttons (Update, View, Delete).
-    }
+        // TODO:Implement OnClickListener for View buttons
+        // Insert the OnClickListener for the "View" button here
+        Button buttonView = findViewById(R.id.buttonView);
+        buttonView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace the fragment with ItemListFragment when "View" button is clicked
 
+                if (isViewItemVisible) {
+                    // Remove the Add Item fragment
+                    removeViewItemListFragment();
+                    Log.d("hello wolrd", "checking Add view List  and hide");
+                } else {
+                    // Show the Add Item fragment
+                    showViewItemListFragment();
+                    Log.d("hello wolrd", "checking add view List and visible ");
+                }
+                isViewItemVisible = !isViewItemVisible;
+            }
+        });
+    }
+    // TODO: Below  For Add item Fragmment Show and Hide layout mehtod
     private void showAddItemFragment() {
        // FragmentManager fragmentManager = getSupportFragmentManager();
        // FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -97,7 +125,100 @@ public class MainActivity extends AppCompatActivity {
             Log.d("hello wolrd", "remove add view");
         }
     }
+    // TODO: Above  For Add item Fragmment Show and Hide layout mehtod
+
+//TODO: Below  For VIew Item Fragment SHow And Hide when Click Buttton View
+    private void showViewItemListFragment() {
+        // Create a new instance of ItemListFragment
+        Fragment itemListFragment = new ItemListFragment();
+        // Create a dummy list of items
+        /*List<Item> itemList = new ArrayList<>();
+        itemList.add(new Item("Item 1", "Description 1", "Age 1"));
+        itemList.add(new Item("Item 2", "Description 2", "Age 2"));
+// Set the data for the ItemListFragment
+        if (itemListFragment instanceof ItemListFragment) {
+            ((ItemListFragment) itemListFragment).setItemList(itemList);
+        }*/
+
+        // Retrieve data from Google Sheets using an HTTP request
+        //String url = "https://script.google.com/macros/s/AKfycbyLiHAb1XdRI1FSOyGTNROOo6oAnenNHc18XRF7IfzOs7TSWMYIE8G4GgRaN8wEkuna/exec?action=getData";
+        String url = "https://script.google.com/macros/s/AKfycbyPx6b7h9XMur9krNwYmqqucKqziMHj0rRS7hXU1HLZZZc8Lj1m4qBIiPHHIu9sjDOBBg/exec";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Parse the JSON response and update the itemListFragment
+                        // Here, you'll need to implement code to populate your itemListFragment with the data.
+                        // You can parse the JSON response and create Item objects from it.
+
+                        // Example parsing:
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("data");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject itemObject = jsonArray.getJSONObject(i);
+                                String name = itemObject.getString("name");
+                                String description = itemObject.getString("description");
+                                String age = itemObject.getString("age");
+
+                                // Create Item objects and add them to your list
+                                Item item = new Item(name, description, age);
+                                itemList.add(item);
+                            }
+                            // Log data retrieval
+                            Log.d("DataRetrieval", "Data retrieved successfully");
+
+                            // Update the itemListFragment with the data
+                            if (itemListFragment != null) {
+                                ((ItemListFragment) itemListFragment).updateItemList(itemList);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            // Log error in JSON parsing
+                            Log.e("DataRetrieval", "Error parsing JSON response");
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle error
+                        Toast.makeText(MainActivity.this, "Error retrieving data", Toast.LENGTH_SHORT).show();
+
+                        Log.e("DataRetrieval", "Error retrieving data");
+                    }
+                });
+
+        // Add the request to the Volley request queue
+        requestQueue.add(jsonObjectRequest);
+
+        // Replace the current fragment with ItemListFragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, itemListFragment)
+                .addToBackStack(null) // If you want to allow back navigation
+                .commit();
+        this.itemListFragment = (ItemListFragment) itemListFragment;
+    }
+    private void removeViewItemListFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
+        if (fragment != null) {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            // Remove the AddItemFragment from the container
+            fragmentTransaction.remove(fragment);
+
+            // Commit the transaction
+            fragmentTransaction.commit();
+            Log.d("hello wolrd", "remove  view Item");
+        }
+    }
+    //TODO: Above For VIew Item Fragment SHow And Hide when Click Buttton View
+
     // Define the sendDataToGoogleSheets method here, similar to how it's done in AddItemActivity
+    // TODO: SendDataToGoogleSheets method here, similar to how it's done in AddItemActivity Send Data to Google Sheet
     public void sendDataToGoogleSheets(JSONObject postData) {
         // Your implementation for sending data to Google Sheets
         RequestQueue requestQueue = Volley.newRequestQueue(this);
@@ -182,33 +303,6 @@ public class MainActivity extends AppCompatActivity {
         requestQueue.add(jsonObjectRequest);
 
     }
-    // TODO: Implement methods for Update, View, and Delete operations.
-/*
-    private Button buttonAdd;
-    private boolean isAddItemVisible = false; // To track the visibility of the Add Item fragment
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // Initialize UI elements
-        buttonAdd = findViewById(R.id.buttonAdd);
-
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Inflate add_item.xml layout as a fragment when "Add" button is clicked
-                Fragment addItemFragment = new AddItemFragment();
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragmentContainer, addItemFragment)
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
-
-        // TODO: Implement OnClickListener for other buttons (Update, View, Delete).
-    }
-
-    // TODO: Implement methods for Update, View, and Delete operations.*/
 }
