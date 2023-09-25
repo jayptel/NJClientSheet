ac51eef4725d55e3a23851f5b04689f24b6c41ce
Acer <Acer@DESKTOP-02PQJEJ>
2023-09-25 11:00:25 +0530

    CompleteAddItem

--- app/src/main/java/com/rhythm/njclienttsheet/AddItemFragment.java
+++ app/src/main/java/com/rhythm/njclienttsheet/AddItemFragment.java
@@ -4,16 +4,88 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.fragment.app.Fragment;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class AddItemFragment extends Fragment {
+
+    private EditText editTextName, editTextDescription, editTextAge;
+    private Button buttonSend;
+
     public AddItemFragment() {
         // Required empty public constructor
+
+    }
+    // Public method to clear the text of editTextName
+    public void clearEditTextName() {
+        if (editTextName != null) {
+            editTextName.getText().clear();
+        }
     }
 
+    // Public method to set focus on the name EditText
+    public void requestFocusOnNameEditText() {
+        if (editTextName != null) {
+            editTextName.requestFocus();
+        }
+    }
+
+    // Public method to clear the text of editTextDescription
+    public void clearEditTextDescription() {
+        if (editTextDescription != null) {
+            editTextDescription.getText().clear();
+        }
+    }
+
+    // Public method to clear the text of editTextAge
+    public void clearEditTextAge() {
+        if (editTextAge != null) {
+            editTextAge.getText().clear();
+        }
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.add_item, container, false);
+       // return inflater.inflate(R.layout.add_item, container, false);
+        View rootView = inflater.inflate(R.layout.add_item, container, false);
+// Initialize UI elements
+        editTextName = rootView.findViewById(R.id.editTextName);
+        editTextDescription = rootView.findViewById(R.id.editTextDescription);
+        editTextAge = rootView.findViewById(R.id.editTextAge);
+        buttonSend = rootView.findViewById(R.id.buttonSend);
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get data from EditText fields
+                String name = editTextName.getText().toString();
+                String description = editTextDescription.getText().toString();
+                String age = editTextAge.getText().toString();
+
+                // Create a JSON object with the data
+                JSONObject postData = new JSONObject();
+                try {
+                    postData.put("action", "addItem");
+                    postData.put("name", name);
+                    postData.put("description", description);
+                    postData.put("age", age);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                // Send the data to Google Sheets API (you can call a method in MainActivity to do this)
+                ((MainActivity) requireActivity()).sendDataToGoogleSheets(postData);
+            }
+        });
+
+        return rootView;
     }
+
+
 }
--- app/src/main/java/com/rhythm/njclienttsheet/MainActivity.java
+++ app/src/main/java/com/rhythm/njclienttsheet/MainActivity.java
@@ -5,15 +5,30 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
     private Button buttonAdd;
-
+   // private Button buttonSend;
+    //private EditText editTextName, editTextDescription, editTextAge;
+   private AddItemFragment addItemFragment;
     private boolean isAddItemVisible = false; // To track the visibility of the Add Item fragment
 
     @Override
@@ -28,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Start the AddItemActivity
-               Intent intent = new Intent(MainActivity.this, AddItemActivity.class);
-               startActivity(intent);
+              // Intent intent = new Intent(MainActivity.this, AddItemActivity.class);
+              // startActivity(intent);
 
                 if (isAddItemVisible) {
                     // Remove the Add Item fragment
@@ -49,20 +64,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showAddItemFragment() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        /*Fragment addItemFragment = new AddItemFragment();
+       // FragmentManager fragmentManager = getSupportFragmentManager();
+       // FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment addItemFragment = new AddItemFragment();
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragmentContainer, addItemFragment)
                 .addToBackStack(null)
-                .commit();*/
-        // Create a new instance of the AddItemFragment and add it to the container
-       AddItemFragment addItemFragment = new AddItemFragment();
-       fragmentTransaction.add(R.id.fragmentContainer, addItemFragment);
+                .commit();
+        // Assign the fragment instance to addItemFragment
+        this.addItemFragment = (AddItemFragment) addItemFragment;
+            // Create a new instance of the AddItemFragment and add it to the container
+            // AddItemFragment addItemFragment = new AddItemFragment();
+            // fragmentTransaction.add(R.id.fragmentContainer, addItemFragment);
 
         //Commit the transaction
-       fragmentTransaction.commit();
+      // fragmentTransaction.commit();
         Log.d("hello wolrd", "add add view ");
+
     }
 
     private void removeAddItemFragment() {
@@ -79,7 +97,91 @@ public class MainActivity extends AppCompatActivity {
             Log.d("hello wolrd", "remove add view");
         }
     }
+    // Define the sendDataToGoogleSheets method here, similar to how it's done in AddItemActivity
+    public void sendDataToGoogleSheets(JSONObject postData) {
+        // Your implementation for sending data to Google Sheets
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        // Replace with your Google Sheets API endpoint URL
+        String apiUrl = "https://script.google.com/macros/s/AKfycbyLiHAb1XdRI1FSOyGTNROOo6oAnenNHc18XRF7IfzOs7TSWMYIE8G4GgRaN8wEkuna/exec";
 
+        // Create a Volley request to send data to the API
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                apiUrl,
+                postData,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Handle the response from Google Sheets (e.g., success message)
+                        try {
+                            String message = response.getString("message");
+                            if ("Success".equals(message)) {
+                                // Handle success, e.g., show a success message to the user
+                                if (addItemFragment != null) {
+                                    Toast.makeText(MainActivity.this, "Text Box clear ", Toast.LENGTH_SHORT).show();
+                                    addItemFragment.clearEditTextName();
+                                    addItemFragment.clearEditTextDescription();
+                                    addItemFragment.clearEditTextAge();
+                                    addItemFragment.requestFocusOnNameEditText();
+                                }
+                                Toast.makeText(MainActivity.this, "Item added successfully", Toast.LENGTH_SHORT).show();
+
+                                // Clear the EditText fields
+                                // Clear the EditText fields in the AddItemFragment
+
+                            } else {
+                                // Handle other cases, e.g., show an error message
+                                Toast.makeText(MainActivity.this, "Server returned an unexpected response", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            // Handle JSON parsing error
+                            Toast.makeText(MainActivity.this, "Error parsing server response", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle errors, including network errors and response errors
+                        Log.e("hello world", "Error sending data", error);
+                        if (error.networkResponse != null) {
+                            String errorResponse = new String(error.networkResponse.data);
+                            Log.e("hello world", "Error response: " + errorResponse);
+
+                            // Check response is JSON before parsing
+                            if (errorResponse.startsWith("{")) {
+                                try {
+                                    JSONObject json = new JSONObject(errorResponse);
+                                    // Handle the JSON response here if needed
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                    // Handle JSON parsing error
+                                    Log.e("Error", "Non-JSON response type: " + errorResponse.substring(0, 20));
+
+                                    Toast.makeText(MainActivity.this, "Error parsing error response", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                // Handle non-JSON response
+                                Log.e("Error", "Non-JSON response type: " + errorResponse.substring(0, 20));
+
+                                Toast.makeText(MainActivity.this, "Received a non-JSON response", Toast.LENGTH_SHORT).show();
+                            }
+                        }else {
+                            // Network error
+                            Log.e("Error", "Network error: " + error.getMessage());
+
+                            Toast.makeText(MainActivity.this, "Network error - please check your internet connection.", Toast.LENGTH_SHORT).show();
+                        }
+                        Toast.makeText(MainActivity.this, "Error sending data", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+// Add the request to the Volley request queue
+
+        requestQueue.add(jsonObjectRequest);
+
+    }
     // TODO: Implement methods for Update, View, and Delete operations.
 /*
     private Button buttonAdd;
